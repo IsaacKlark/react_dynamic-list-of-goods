7ffc5cb48d9059978bec1e8bf60020e7c8538a96
IsaacKlark <isaackeklarke@gmail.com>
2019-12-18 19:54:13 +0200

    Updates

---
+++ precache-manifest.2709d349a1ec5cb838367d2419106bdc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "071172d28253c28a5aa6affef1b37174",
+    "url": "/react_dynamic-list-of-goods/index.html"
+  },
+  {
+    "revision": "8b489006b64d3839c97d",
+    "url": "/react_dynamic-list-of-goods/static/css/main.18a722b9.chunk.css"
+  },
+  {
+    "revision": "d499d87a993599493776",
+    "url": "/react_dynamic-list-of-goods/static/js/2.1e4a2dbe.chunk.js"
+  },
+  {
+    "revision": "8b489006b64d3839c97d",
+    "url": "/react_dynamic-list-of-goods/static/js/main.aa65bb63.chunk.js"
+  },
+  {
+    "revision": "c4cd083d622a7c9ffb38",
+    "url": "/react_dynamic-list-of-goods/static/js/runtime~main.6d24ca6d.js"
+  }
+]);
